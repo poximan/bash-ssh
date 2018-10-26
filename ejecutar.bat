dd7bc714cae1914ae86b3222122758b7ee65d001
poximan <poxi_man@yahoo.com>
2018-10-26 18:37:27 -0300

    agregando comandos faltantes para arranque docker

--- ejecutar.bat
+++ ejecutar.bat
@@ -1,2 +1,4 @@
 docker build -t img-alpineb .
-docker run -ti --name cont-alpineb img-alpineb bash
+docker run --name cont-alpineb img-alpineb
+docker start cont-alpineb
+docker exec -ti cont-alpineb bash
